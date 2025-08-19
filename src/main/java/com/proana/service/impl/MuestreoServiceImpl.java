645dc7882b83582936f9daf231ce921b7427cfa6
Ezequiel <ediprospero@gmail.com>
2025-08-19 18:15:08 -0300

    se mapea mejor respuesta de get muestreos

--- src/main/java/com/proana/service/impl/MuestreoServiceImpl.java
+++ src/main/java/com/proana/service/impl/MuestreoServiceImpl.java
@@ -1,6 +1,9 @@
 package com.proana.service.impl;
 
+import com.proana.dto.ItemDTO;
+import com.proana.dto.MonedaDto;
 import com.proana.dto.MuestreoDTO;
+import com.proana.model.Muestra;
 import com.proana.model.Muestreo;
 import com.proana.repository.MuestreoRepository;
 import com.proana.service.MuestreoService;
@@ -35,7 +38,7 @@ public class MuestreoServiceImpl implements MuestreoService {
             logger.info("Inicio de la obtención de muestreos");
             List<Muestreo> entidades = this.muestreoRep.findAll();
             List<MuestreoDTO> dtos = entidades.stream()
-                    .map(ent -> new MuestreoDTO(ent.getIdMuestreo(), ent.getMuestra(),
+                    .map(ent -> new MuestreoDTO(ent.getIdMuestreo(), mapearItem(ent.getMuestra()),
                             ent.getUbicacion(), ent.getFechaEstimada(), ent.getCantidadMinima(),
                             ent.getUnidad(), ent.getMuestreadores(), ent.getTiempoTotal(),
                             ent.getConsumibles(), ent.getPrecioMuestreo()))
@@ -46,6 +49,28 @@ public class MuestreoServiceImpl implements MuestreoService {
             logger.error("Error al obtener Muestreos", e);
             throw new RuntimeException("No se pudo obtener las muestreos", e);
         }
+    }
+
+    public ItemDTO mapearItem(Muestra muestra) {
+
+        ItemDTO item = new ItemDTO();
+        // Item
+        if (muestra.getIdMuestra() != null) {
+            item.setId(muestra.getIdMuestra());
+            item.setTitulo(muestra.getTitulo());
+            item.setReferencia(muestra.getReferenciaNormativa().getId());
+            item.setMatriz(muestra.getMatriz().getId());
+            item.setPe(muestra.getPe());
+            item.setVeces(muestra.getCantidadVeces());
+            item.setFrecuencia(muestra.getFrecuencia());
+            item.setMuestras(muestra.getCantidadMuestras());
+            item.setOos(muestra.getOos());
+            item.setRoos(muestra.getRoos());
+            item.setSCrudos(muestra.getSCrudos());
+            //item.setDeterminaciones(muestra.getMatriz());
+        }
+
+        return item;
     }
 
 }
